faef79d7973a50d1dbe8f1a6d6fb501cf0183560
ari <frummer28@gmail.com>
2024-12-03 14:26:59 -0500

    evaluatio skeleton

--- src/evaluate_sep_enh_pipeline.py
+++ src/evaluate_sep_enh_pipeline.py
@@ -16,9 +16,35 @@ def main():
 
     # Step 2: Loop over each entry
     for index, entry in enumerate(metadata):
-        # Step 3: Call transcribe_and_evaluate for each entry
-        # transcribe_and_evaluate(entry)
+        # Step 3:
+        # Step 3.1 load data to ram:
+        # load id from entry
+        # load - reference and transcriptions
+        # load gt1_name
+        # load gt1_audio_file
+        # load gt1_transcription
+        # load gt2_name
+        # load gt2_audio_file
+        # load gt2_transcription
+        # load - separated audio1
+        # load - separated audio2
+
+        # Step 3.2 transcribe all audios:
+        # transcribe gt1
+        # transcribe gt2
+        # transcribe gt1_audio_file
+        # transcribe gt2_audio_file
+
+        # Step 3.3 calculate WER:
+        # transcribe gt1 vs transcription_g1
+        # transcribe gt2 vs transcription_g2
+        # transcribe gt1_audio_file vs transcription_g1
+        # transcribe gt1_audio_file vs transcription_g2
+        # transcribe gt2_audio_file vs transcription_g1
+        # transcribe gt2_audio_file vs transcription_g2
+
         print(f"index:{index}")
+        break
 
 
 if __name__ == "__main__":
